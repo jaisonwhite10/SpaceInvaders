565f535e70056712d676c028e70ecdb7a41c4cad
jaisonwhite10 <whitejaison@gmail.com>
2023-11-05 16:14:16 -0500

    Added Aliens.py, Added GIF Images of all Characters Started movement of aliens in game

--- aliens.py
+++ aliens.py
@@ -1,4 +1,84 @@
 from turtle import Turtle
 
 class Aliens(Turtle):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.speed('fastest')
+        self.x_positions = [-250,-200,-150,-100,-50,0,50,100,150,200,250]
+        self.list_of_enemies = [[],[],[],[],[]]
+
+
+    def create_alien(self, image, y_coord,enemy_index):
+
+        for alien_index in range(0, 11):
+            alien = Aliens()
+            alien.shape(image)
+            alien.goto(self.x_positions[alien_index], y_coord)
+            self.list_of_enemies[enemy_index].append(alien)
+
+    def create_alien_ship(self,image,ycoord):
+        alien = Aliens()
+        alien.shape(image)
+        alien.goto(0,ycoord)
+    # def move_aliens(self):
+    #
+    #     for list in self.list_of_enemies:
+    #         # max_alien = max(list, key=lambda alien: alien.xcor())
+    #         # min_alien = min(list, key=lambda alien: alien.xcor())
+    #         # if max_alien.xcor() > 270:
+    #         #     for alien in list:
+    #         #         self.move_left(alien)
+    #         # elif min_alien.xcor() < -270:
+    #         #     for alien in list:
+    #         #         self.move_right(alien)
+    #
+    #         ###mine
+    #         list_of_x_coordinate = [alien.xcor() for alien in list]
+    #         max_x = max(list_of_x_coordinate)
+    #         min_x = min(list_of_x_coordinate)
+    #         for alien in list:
+    #             self.move_right(alien)
+    #         #     # max_x = alien.xcor().max()
+    #         #     # self.move_left(alien)
+    #             if max_x > 270:
+    #                 self.move_left(alien)
+    #             elif min_x < -270:
+    #                 self.move_right(alien)
+    #
+    #
+    #         # ###mine
+
+    def move_aliens(self):
+        for list in self.list_of_enemies:
+            # list_of_x_coordinate = [alien.xcor() for alien in list]
+            # max_x = max(list_of_x_coordinate)
+            # min_x = min(list_of_x_coordinate)
+            for alien in list:
+                alien.forward(0.1)
+            self.check_borders()
+
+
+
+
+    def check_borders(self):
+
+        for list in self.list_of_enemies:
+
+            right_screen = any(alien.xcor() > 370 for alien in list)
+            left_screen = any(alien.xcor() < -370 for alien in list)
+            for alien in list:
+                if right_screen:
+                    alien.setheading(180)
+                elif left_screen:
+                    alien.setheading(0)
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,54 @@
 from turtle import Turtle, Screen
 from spaceship import Spaceship
+from aliens import Aliens
 from tkinter import *
 
 #adds ship image to Turtle Screen
 ship_image = "images/ship.gif"
 
+jelly_fish_image = "images/jelly_fish.gif"
+
+crab_alien_image = "images/crab_alien3.gif"
+
+squid_alien_image = "images/squid_alien.gif"
+
+alien_ship_image = "images/alien_ship.gif"
 screen = Screen()
 
 #adds spaceship image to Screen
 screen.addshape(ship_image)
-
+screen.addshape(jelly_fish_image)
+screen.addshape(crab_alien_image)
+screen.addshape(squid_alien_image)
+screen.addshape(alien_ship_image)
 ## Creates a ship and sets it at the defined x,y coordinates
-ship = Spaceship((0,-220))
+ship = Spaceship((0,-250))
 
 #adds ship image to spaceship Turtle
 ship.shape(ship_image)
 
+alien = Aliens()
+#Jellyfish Aliens
+alien.create_alien(image=jelly_fish_image,y_coord=30,enemy_index=0)
+alien.create_alien(image=jelly_fish_image,y_coord=70,enemy_index=1)
+
+#Crab Aliens
+alien.create_alien(image=crab_alien_image,y_coord=110,enemy_index=2)
+alien.create_alien(image=crab_alien_image,y_coord=150,enemy_index=3)
+
+#Jellyfish Alien
+alien.create_alien(image=squid_alien_image,y_coord=190,enemy_index=4)
+
+#Alien Ship
+alien.create_alien_ship(image=alien_ship_image,ycoord=270)
+
+
+
+
+
+
 screen.bgcolor("black")
-screen.setup(width=700,height=700)
+screen.setup(width=800,height=800)
 screen.title('Space Invaders')
 screen.tracer(0)
 screen.listen()
@@ -30,6 +61,7 @@ screen.onkeypress(fun=ship.shoot_lazer,key='space')
 
 
 while True:
+    alien.move_aliens()
     screen.update()
 
 
